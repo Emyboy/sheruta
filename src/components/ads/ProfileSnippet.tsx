724a75288569d15a0ddbe74e85cd2dc9c4924bc0
dLuxKid <adetunjimarvellous09@gmail.com>
2024-10-01 15:15:38 +0100

    removed clear filter button

--- src/components/ads/ProfileSnippet.tsx
+++ src/components/ads/ProfileSnippet.tsx
@@ -12,7 +12,7 @@ import {
 	Stack,
 	Text,
 } from '@chakra-ui/react'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 import React from 'react'
 import { BiBookmark } from 'react-icons/bi'
 import { TbCircleLetterX } from 'react-icons/tb'
@@ -49,26 +49,29 @@ interface UserProfile {
 const ProfileSnippet = ({ userProfiles }: Props) => {
 	const parsedUserProfile: UserProfile[] = JSON.parse(userProfiles)
 	const router = useRouter()
+	const params = useSearchParams()
 
 	return (
 		<React.Fragment>
-			<Button
-				w={'100%'}
-				gap={2}
-				mb={'-1rem'}
-				p={0}
-				pr={3}
-				justifyContent={'end'}
-				fontSize={{ base: 'sm', md: 'base' }}
-				fontWeight={300}
-				color={'brand'}
-				bgColor={'transparent'}
-				_hover={{ bgColor: 'transparent', textDecoration: 'none' }}
-				onClick={() => router.push('/')}
-			>
-				Clear Filters
-				<TbCircleLetterX size={'16px'} />
-			</Button>
+			{params.toString() && (
+				<Button
+					w={'100%'}
+					gap={2}
+					mb={'-1rem'}
+					p={0}
+					pr={3}
+					justifyContent={'end'}
+					fontSize={{ base: 'sm', md: 'base' }}
+					fontWeight={300}
+					color={'brand'}
+					bgColor={'transparent'}
+					_hover={{ bgColor: 'transparent', textDecoration: 'none' }}
+					onClick={() => router.push('/')}
+				>
+					Clear Filters
+					<TbCircleLetterX size={'16px'} />
+				</Button>
+			)}
 
 			{parsedUserProfile.length > 0 ? (
 				parsedUserProfile.map((item, index) => {
