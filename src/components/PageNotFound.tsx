70c65756121ab9081b0768fe379dd1d91addd881
dLuxKid <adetunjimarvellous09@gmail.com>
2024-10-08 17:31:30 +0100

    added 404 page

--- src/components/PageNotFound.tsx
+++ src/components/PageNotFound.tsx
@@ -1,8 +1,11 @@
 import { NAV_HEIGHT } from '@/configs/theme'
-import { Flex, Text } from '@chakra-ui/react'
+import { Button, Flex, Text } from '@chakra-ui/react'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export default function PageNotFound() {
+	const router = useRouter()
+
 	return (
 		<Flex
 			minH={`calc(90vh - ${NAV_HEIGHT})`}
@@ -12,6 +15,14 @@ export default function PageNotFound() {
 		>
 			<Text fontSize={'100px'}>404</Text>
 			<Text>Page Not Found</Text>
+			<Flex gap={2} alignItems={'center'} justifyContent={'center'} mt={2}>
+				<Button bg={'dark'} color={'white'} onClick={() => router.back()}>
+					Go Back
+				</Button>
+				<Button bg={'brand'} color={'white'} onClick={() => router.push('/')}>
+					Home
+				</Button>
+			</Flex>
 		</Flex>
 	)
 }
