f0b7a3cf97c378fe1474af53092d2ce7fb331606
Octagon <ugorji757@gmail.com>
2024-10-25 02:34:21 +0300

    chore: refactored header bubbles

--- src/hooks/useHeaderBubbles.tsx
+++ src/hooks/useHeaderBubbles.tsx
@@ -6,45 +6,45 @@ import { useState, useEffect } from 'react'
 import { ConversationData } from '@/firebase/service/conversations/conversations.types'
 
 const useHeaderBubbles = () => {
-	const {
-		authState: { user },
-	} = useAuthContext()
+    const {
+        authState: { user },
+    } = useAuthContext()
 
-	const axiosInstance = useAuthenticatedAxios()
+    const axiosInstance = useAuthenticatedAxios()
 
-	const [bubbles, setBubbles] = useState({
-		messages: false,
-		//add others here
-	})
+    const [bubbles, setBubbles] = useState({
+        messages: false,
+        //add others here
+    })
 
-	useEffect(() => {
-		if (user) {
-			const getConversations = async () => {
-				if (user) {
-					if (!axiosInstance) {
-						return null
-					}
+    useEffect(() => {
+        const getConversations = async () => {
+            try {
+                if (!(axiosInstance && user)) {
+                    return null
+                }
 
-					const {
-						data: { conversations: userConversations },
-					}: {
-						data: { conversations: ConversationData[] }
-					} = await axiosInstance.get(`/conversations`)
+                const {
+                    data: { conversations: userConversations },
+                }: {
+                    data: { conversations: ConversationData[] }
+                } = await axiosInstance.get(`/conversations`)
 
-					setBubbles((prev) => ({
-						...prev,
-						messages: userConversations.some(
-							(conversation) => conversation?.unread_messages !== 0,
-						),
-					}))
-				}
-			}
+                setBubbles((prev) => ({
+                    ...prev,
+                    messages: userConversations.some(
+                        (conversation) => conversation?.unread_messages !== 0,
+                    ),
+                }))
+            } catch (err) {
+                console.log(err)
+            }
+        }
 
-			getConversations()
-		}
-	}, [user, axiosInstance])
+        getConversations()
+    }, [user, axiosInstance])
 
-	return { bubbles }
+    return { bubbles }
 }
 
 export default useHeaderBubbles
